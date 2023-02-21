c6305c1faca56bf1b729766f9816be1dae171a2b
AshwiniTagadghar1 <ashwinitagadghar7@gmail.com>
2023-02-21 20:51:09 +0530

    working code

--- index.js
+++ index.js
@@ -173,88 +173,83 @@ const authHandler = async (
   authenticationUrl
 ) => {
   try {
-    await UserAuth.findOne({ viberId: viberUserId }, (err, userExist) => {
-      if (err) return console.error(err);
-      if (userExist) {
-        // validating through token expiry
-        const expires_in = userExist.token_expires_in;
-        const issued_at = userExist.token_issued_at;
-        const expirationTime = expires_in + issued_at; // Expiration time in seconds
+    const userExist = await UserAuth.findOne({ viberId: viberUserId });
+    if (userExist) {
+      // validating through token expiry
+      const expires_in = userExist.token_expires_in;
+      const issued_at = userExist.token_issued_at;
+      const expirationTime = expires_in + issued_at; // Expiration time in seconds
 
-        const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
-        const isExpired = currentTime > expirationTime ? true : false; // check for token expiry
+      const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+      const isExpired = currentTime > expirationTime ? true : false; // check for token expiry
 
-        if (isExpired) {
-          // user has to reauthenticate, and update user_auth in db!
-          UserAuth.findOneAndUpdate({ viberId: viberUserId }, { $set: { is_expired: true } }, { new: true }, (err, updatedDoc) => {
-            if (err) return console.error(err);
-            console.log("Document updated: ", updatedDoc);
-          });
+      if (isExpired) {
+        // user has to reauthenticate, and update user_auth in db!
+        await UserAuth.findOneAndUpdate({ viberId: viberUserId }, { $set: { is_expired: true } }, { new: true });
 
-          client.sendText(
-            user.id,
-            `Hi ${viberUserName}, Please login using this link: ${authenticationUrl} and then type "register" to register your Github repo for pull request notifications! `
-            // customkeyboard
-          );
-        } else {
-          // user is still authenticated, user might have unsubscribed, and resubscribed, so can directly tell user to proceed to register his repo!
-          if (event === "conversation_started" || event === "subscribed") {
+        await client.sendText(
+          user.id,
+          `Hi ${viberUserName}, Please login using this link: ${authenticationUrl} and then type "register" to register your Github repo for pull request notifications! `
+          // customkeyboard
+        );
+      } else {
+        // user is still authenticated, user might have unsubscribed, and resubscribed, so can directly tell user to proceed to register his repo!
+        if (event === "conversation_started" || event === "subscribed") {
+          try {
+            await client.sendText(
+              viberUserId,
+              `Welcome back ${viberUserName}! You are already logged in! Please type "register" to register your Github repo for pull request notifications! `
+            );
+          } catch (err) {
+            console.log(err);
+          }
+        } else if (event === "message") {
+          if (message.text.toLowerCase() === "register") {
             try {
-              client.sendText(
+              await client.sendText(
                 viberUserId,
-                `Welcome back ${viberUserName}! You are already logged in! Please type "register" to register your Github repo for pull request notifications! `
+                'Please provide your details in the following format: "details GitHub_Access_Token UserName RepoName"'
               );
             } catch (err) {
               console.log(err);
             }
-          } else if (event === "message") {
-            if (message.text.toLowerCase() === "register") {
-              try {
-                client.sendText(
-                  viberUserId,
-                  'Please provide your details in the following format: "details GitHub_Access_Token UserName RepoName"'
-                );
-              } catch (err) {
-                console.log(err);
-              }
-            } else if (message.text.toLowerCase().startsWith("details")) {
+          } else if (message.text.toLowerCase().startsWith("details")) {
 
-              const accessToken = message.text.split(" ")[1];
-              const repoOwner = message.text.split(" ")[2];
-              const repoName = message.text.split(" ")[3];
-              try {
-                setGithubWebhook(
-                  viberUserId,
-                  viberUserName,
-                  accessToken,
-                  repoOwner,
-                  repoName
-                );
-              } catch (err) {
-                console.log(err);
-              }
-            } else {
-              // echo user
-              try {
-                client.sendText(
-                  viberUserId,
-                  `Hello ${viberUserName}, you said: ${message.text}`
-                );
-              } catch (err) {
-                console.log(err);
-              }
+            const accessToken = message.text.split(" ")[1];
+            const repoOwner = message.text.split(" ")[2];
+            const repoName = message.text.split(" ")[3];
+            try {
+              await setGithubWebhook(
+                viberUserId,
+                viberUserName,
+                accessToken,
+                repoOwner,
+                repoName
+              );
+            } catch (err) {
+              console.log(err);
+            }
+          } else {
+            // echo user
+            try {
+              await await client.sendText(
+                viberUserId,
+                `Hello ${viberUserName}, you said: ${message.text}`
+              );
+            } catch (err) {
+              console.log(err);
             }
           }
         }
-      } else {
-        // user not exist so need to authenticated and store in DB
-        client.sendText(
-          viberUserId,
-          `Hi ${viberUserName}, Welcome to our Chatbot! Please login here: ${authenticationUrl} and then type "register" to register your Github repo for pull request notifications! `
-          // customkeyboard
-        );
       }
-    });
+    } else {
+      // user not exist so need to authenticated and store in DB
+      await client.sendText(
+        viberUserId,
+        `Hi ${viberUserName}, Welcome to our Chatbot! Please login here: ${authenticationUrl} and then type "register" to register your Github repo for pull request notifications! `
+        // customkeyboard
+      );
+    }
   } catch (err) {
     console.log(err);
   }
